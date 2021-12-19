b28538aa5ccc77b1182e37e1ebebdd3028c0f9d8
Alessandro <alessandro01desideri@gmail.com>
2021-12-19 17:03:06 +0100

    Level Variables, Grid dimensions

---
+++ js/script.js
@@ -0,0 +1,22 @@
+// Dichiarazione variabili
+
+// Livello facile
+const easy = document.querySelector('.easy-level');
+const easyGrid = 100;
+
+// Livello medio
+const medium = document.querySelector('.medium-level');
+const mediumGrid = 81;
+
+// Livello difficile
+const hard = document.querySelector('.hard-level');
+const hardGrid = 49;
+
+// Contenitore dei 'box'
+const gridContainer = document.querySelector('.grid-container');
+
+// Variabili per la scelta del numero di
+// 'box' contenuti nel container
+let easyDifficulty = false;
+let mediumDifficulty = false;
+let hardDifficutly = false;
